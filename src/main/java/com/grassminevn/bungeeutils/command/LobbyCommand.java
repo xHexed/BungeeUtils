6e1d8a3e3fef40bdc68fe389acfb9fd8075f7c45
xHexed <minhquanpct7b@gmail.com>
2021-05-19 21:55:57 +0700

    Cache lobby server info

--- src/main/java/com/grassminevn/bungeeutils/command/LobbyCommand.java
+++ src/main/java/com/grassminevn/bungeeutils/command/LobbyCommand.java
@@ -2,18 +2,22 @@ package com.grassminevn.bungeeutils.command;
 
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class LobbyCommand extends Command {
+    private final ServerInfo lobbyServer;
+
     public LobbyCommand() {
         super("lobby", null, "hub");
+        lobbyServer = ProxyServer.getInstance().getServerInfo("lobby");
     }
 
     @Override
     public void execute(final CommandSender sender, final String[] args) {
         if (!(sender instanceof ProxiedPlayer)) return;
         final ProxiedPlayer player = (ProxiedPlayer) sender;
-        player.connect(ProxyServer.getInstance().getServerInfo("lobby"));
+        player.connect(lobbyServer);
     }
 }
