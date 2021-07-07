7d12560e9523756c6bc100da68d61ac389a533b9
xHexed <minhquanpct7b@gmail.com>
2021-07-07 09:55:34 +0700

    Add ignore words option and modify server mod info

--- src/main/java/com/grassminevn/bungeeutils/EventManager.java
+++ src/main/java/com/grassminevn/bungeeutils/EventManager.java
@@ -1,40 +1,50 @@
 package com.grassminevn.bungeeutils;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.connection.Server;
+import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class EventManager implements Listener {
-    private final ServerInfo lobby;
-    private final BaseComponent[] disconnectHeaderMessage;
-    private final BaseComponent[] disconnectFooterMessage;
+    private BungeeUtils plugin;
 
     EventManager(final BungeeUtils plugin) {
-        lobby  = ProxyServer.getInstance().getServerInfo(plugin.getConfigManager().getConfig().getString("lobby-server", "lobby"));
-        disconnectHeaderMessage = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', plugin.getConfigManager().getConfig().getString("messages.disconnect.header")));
-        disconnectFooterMessage = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', plugin.getConfigManager().getConfig().getString("messages.disconnect.footer")));
+        this.plugin = plugin;
     }
 
     @EventHandler
     public void onPlayerDisconnect(final ServerKickEvent event) {
         final ProxiedPlayer player = event.getPlayer();
         final Server server = player.getServer();
-        if (event.getKickedFrom() != lobby && server != null && !server.getInfo().equals(lobby)) {
+        for (BaseComponent component : event.getKickReasonComponent()) {
+            if (plugin.getConfigManager().ignoreWords.contains(component.toLegacyText())) {
+                return;
+            }
+        }
+        if (event.getKickedFrom() != plugin.getConfigManager().lobbyServer && server != null && !server.getInfo().equals(plugin.getConfigManager().lobbyServer)) {
             event.setCancelled(true);
-            event.setCancelServer(lobby);
-            player.sendMessage(disconnectHeaderMessage);
+            event.setCancelServer(plugin.getConfigManager().lobbyServer);
+            player.sendMessage(plugin.getConfigManager().disconnectHeaderMessage);
             for (final BaseComponent component : event.getKickReasonComponent()) {
                 player.sendMessage(new TextComponent(TextComponent.fromLegacyText(
                         ChatColor.translateAlternateColorCodes('&', component.toLegacyText()))));
             }
-            player.sendMessage(disconnectFooterMessage);
+            player.sendMessage(plugin.getConfigManager().disconnectFooterMessage);
         }
+    }
+
+    @EventHandler(priority = 64)
+    public void onProxyPing(ProxyPingEvent e) {
+        if (e.getResponse() == null)
+            return;
+        ServerPing ping = e.getResponse();
+        ping.getModinfo().setType(plugin.getConfigManager().serverType);
+        e.setResponse(ping);
     }
 }
--- src/main/java/com/grassminevn/bungeeutils/config/ConfigManager.java
+++ src/main/java/com/grassminevn/bungeeutils/config/ConfigManager.java
@@ -1,5 +1,10 @@
 package com.grassminevn.bungeeutils.config;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
@@ -9,11 +14,19 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ConfigManager {
     private Configuration config;
     private final Plugin plugin;
     private final File configFile;
+
+    public ServerInfo lobbyServer;
+    public BaseComponent[] disconnectHeaderMessage;
+    public BaseComponent[] disconnectFooterMessage;
+    public Set<String> ignoreWords;
+    public String serverType;
 
     public ConfigManager(final Plugin plugin) {
         this.plugin = plugin;
@@ -30,6 +43,11 @@ public class ConfigManager {
         } catch (final IOException e) {
             throw new RuntimeException("Unable to load configuration", e);
         }
+        lobbyServer  = ProxyServer.getInstance().getServerInfo(config.getString("lobby-server", "lobby"));
+        disconnectHeaderMessage = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', config.getString("messages.disconnect.header")));
+        disconnectFooterMessage = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', config.getString("messages.disconnect.footer")));
+        ignoreWords = new HashSet<>(config.getStringList("messages.ignore-words"));
+        serverType = config.getString("server-type");
     }
 
     public void saveDefaultConfig() {
